541e7664bff8b608b55c96900405f7668f8a4367
ilham bouaik <ibouaik@gmail.com>
2022-12-14 17:36:52 +0000

    generate an image with openai

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { Configuration, OpenAIApi } from "openai";
+import {useState} from 'react'
 import './App.css';
 
 
 function App() {
-  console.log(process.env.REACT_APP_OPENAI_API_KEY)
+  // console.log(process.env.REACT_APP_OPENAI_API_KEY)
+  const [prompt, setPrompt] = useState();
+  const [image, setimage] = useState();
+
   const configuration = new Configuration({
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
   });
+  const openai = new OpenAIApi(configuration);
+  const generateImages = async () => {
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+    setimage(response.data.data[0].url)
+  }
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h2>Generate Images</h2>
+        <input onChange={(e) => setPrompt(e.target.value)}></input>
+        <button onClick={generateImages}>Generate an image</button>
+        <img alt="" src={image} /> 
       </header>
     </div>
   );
