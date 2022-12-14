477bbc5986e73e6506d6b85694a288d3a9ca25b2
ilham bouaik <ibouaik@gmail.com>
2022-12-14 16:13:49 +0000

    install openai and generate api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { Configuration, OpenAIApi } from "openai";
 import './App.css';
 
+
 function App() {
+  console.log(process.env.REACT_APP_OPENAI_API_KEY)
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
   return (
     <div className="App">
       <header className="App-header">
